a70ef5c4f50ab7ad73f463258ed3fb8e302b0405
vilas chauvhan <vilas.chauvhan@seaflux.tech>
2024-09-01 15:42:06 +0530

    feat: background animation

--- src/components/organisms/heroSection/index.tsx
+++ src/components/organisms/heroSection/index.tsx
@@ -29,23 +29,33 @@ export default function HeroSection() {
 		});
 	});
 	return (
-		<section className="flex flex-col justify-center items-center gap-y-10 w-screen h-screen">
-			<HeroText heading={heading} />
-			<p
-				id="intro"
-				className="w-1/2 font-inter text-2xl text-center text-white"
+		<section className="relative w-screen h-screen">
+			<div className="z-40 flex flex-col justify-center items-center gap-y-10 mt-auto w-screen h-screen">
+				<HeroText heading={heading} />
+				<p
+					id="intro"
+					className="z-50 w-1/2 font-inter text-2xl text-center text-white"
+				>
+					Our fully automated proprietary quantitative trading software provides
+					24/7 liquidity to 170+ crypto assets across 25+ centralized spot and
+					derivative crypto exchanges.
+				</p>
+				<button
+					id="get-in-touch"
+					type="button"
+					className="z-50 bg-gradient-to-br from-violet-400 via-blue-400 to-orange-300 mt-10 px-8 py-2 rounded-xl h-16 font-inter font-semibold text-2xl text-white uppercase"
+				>
+					Get in touch
+				</button>
+			</div>
+			<video
+				className="bottom-0 -left-10 z-0 absolute bg-transparent p-0 object-fill"
+				autoPlay
+				loop
+				muted
 			>
-				Our fully automated proprietary quantitative trading software provides
-				24/7 liquidity to 170+ crypto assets across 25+ centralized spot and
-				derivative crypto exchanges.
-			</p>
-			<button
-				id="get-in-touch"
-				type="button"
-				className="bg-gradient-to-br from-violet-400 via-blue-400 to-orange-300 mt-10 px-8 py-2 rounded-xl h-16 font-inter font-semibold text-2xl text-white uppercase"
-			>
-				Get in touch
-			</button>
+				<source src={"/animation/hero-animation.mp4"} type="video/mp4" />
+			</video>
 		</section>
 	);
 }
